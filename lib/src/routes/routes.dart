3d344117644d6bcdcc378516dde9cc75c5694cea
Digna Marquez <marquezdigna83@gmail.com>
2021-03-09 09:01:33 -0400

    feat: nuevos componentes de avatar card input y animacion

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,8 @@
 import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:componentes/src/pages/slider_page.dart';
 import 'package:componentes/src/pages/todo_list_page.dart';
 import 'package:flutter/material.dart';
@@ -12,5 +16,9 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'todolist': (BuildContext context) => TodoListScreen(),
     'slider': (BuildContext context) => SliderPage(),
+    'avatar': (BuildContext context) => AvatarPage(),
+    'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage(),
   };
 }
